731853ab726b9a1c07ba1dbf7821a27441875989
Kassian Wren <the@nodebotani.st>
2023-07-25 09:14:28 -0500

    Cleaning up to start again

--- cassandra-kafka-iot-cluster/pi-health-monitor-daemon/health-info.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-
-const temp = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8')
-console.log(temp/1000)
---
+++ cassandra-kafka-iot-cluster/stage-1/populate-ssh-keys.sh
@@ -0,0 +1,12 @@
+for ipend in 167 124 32 195 82 29
+do
+  sshpass -f orange-pi-password.txt ssh-copy-id -o StrictHostKeyChecking=no orangepi@192.168.132.${ipend}
+
+done
+for index in 1 2 3
+do
+	for type in a w
+	do
+		sshpass -f raspi-password.txt ssh-copy-id -o StrictHostKeyChecking=no pi@nodebotanist-cluster-${type}${index}.local
+	done
+done
